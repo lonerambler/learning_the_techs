a93a4552fe050619466d07c3f42a5fea747c8222
Miguel Torres <lonerambler@gmail.com>
2021-01-07 17:21:31 -0600

    Merge PS's guide to C# Testing on VS Code repo
    Guide URL:
    https://app.pluralsight.com/guides/testing-.net-core-apps-with-visual-studio-code
    
    Repo original URL:
    https://github.com/douglasstarnes/ps-guide-net-core-testing

---
+++ CSharp/ps-guide-net-core-testing/NetCoreTesting.Core/PasswordGenerator.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Text;
+
+namespace NetCoreTesting.Core
+{
+    public class PasswordGenerator
+    {
+        public int MinLength { get; set; }
+
+        public PasswordGenerator(int minLength = 8)
+        {
+            MinLength = minLength;
+        }
+
+        public string Generate()
+        {
+            var chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            chars += chars.ToLower();
+            chars += "0123456789";
+
+            var rnd = new Random();
+
+            var password = new StringBuilder();
+
+            for (var i = 0; i < MinLength; i += 1)
+            {
+                var position = rnd.Next(0, chars.Length);
+                password.Append(chars[position]); 
+            }
+
+            return password.ToString();
+        }
+    }
+}
